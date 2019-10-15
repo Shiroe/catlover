c05c43d66fd2544a1c8717bf5198aaecfd7c5467
Shiroe <aggelosteck@gmail.com>
2019-10-15 23:57:23 +0300

    Updated Webpack configurations

---
+++ webpack.config.prod.js
@@ -0,0 +1,16 @@
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.base');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = merge(baseConfig, {
+    mode: 'production',
+    plugins: [new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false,
+        reportFilename: 'bundleAnalysis.html'
+    })],
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM'
+    }
+});
