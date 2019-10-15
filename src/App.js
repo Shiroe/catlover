386e0e42243c4bfc904c007d834a471d3ccb972c
Shiroe <aggelosteck@gmail.com>
2019-10-15 23:58:21 +0300

    Updated package.json for babel / react / loaders / polyfil and webpack devserver

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+class App extends React.Component {
+    state = {
+        count: 0
+    };
+
+    render() {
+        const { count } = this.state;
+
+        return (
+            <div>
+                <h1>CatLover App</h1>
+                <div>Count: {count}</div>
+                <button onClick={() => this.setState({ count: count + 1 })}>+</button>
+                <button onClick={() => this.setState({ count: count - 1 })}>-</button>
+            </div>
+        );
+    }
+}
+
+export default hot(module)(App);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@babel/polyfill';
+
+import App from './App';
+import './styles.css';
+
+ReactDOM.render(<App />, document.getElementById('app'));
