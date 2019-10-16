1ac07b6d282756e8d6ab2479a77a1a5bfce6f026
Shiroe <aggelosteck@gmail.com>
2019-10-17 01:32:05 +0300

    Re-arranged file structure and updated some imports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@babel/polyfill';
 
-import App from './App';
-import './styles.css';
+import App from './components/App/App';
+import './index.css';
 
 if (process.env.NODE_ENV === 'development') {
   const axe = require('react-axe');
