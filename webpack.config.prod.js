f7ffbde87c2f783f38ea76b831e22609fece1a00
Shiroe <aggelosteck@gmail.com>
2019-10-16 22:24:19 +0300

    Run prettier from npx

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,14 +3,16 @@ const baseConfig = require('./webpack.config.base');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = merge(baseConfig, {
-    mode: 'production',
-    plugins: [new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        openAnalyzer: false,
-        reportFilename: 'bundleAnalysis.html'
-    })],
-    externals: {
-        react: 'React',
-        'react-dom': 'ReactDOM'
-    }
+  mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      reportFilename: 'bundleAnalysis.html'
+    })
+  ],
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  }
 });
