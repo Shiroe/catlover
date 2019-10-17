212f93a7b8292f8a1c1d97d35cc21516e16cd999
Shiroe <aggelosteck@gmail.com>
2019-10-17 03:04:59 +0300

    Added node-sass, sass-loader with webpack adjustments and .jsx resolver

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -10,16 +10,19 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         loader: 'babel-loader',
         exclude: /node_modules/
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
         exclude: /node_modules/
       }
     ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
   },
   plugins: [
     new HtmlWebpackPlugin({
