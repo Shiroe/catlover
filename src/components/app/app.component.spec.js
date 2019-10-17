b056308449cbba296579cfbac028a8655c54c3c1
Shiroe <aggelosteck@gmail.com>
2019-10-17 04:39:59 +0300

    Fixing case-insensitive folder name

--- src/components/app/app.component.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './app.component';
-
-describe('App', () => {
-  it('Renders without errors', () => {
-    render(<App />);
-  });
-});
--- src/components/app/index.js
+++
@@ -1,3 +0,0 @@
-import App from './app.component.jsx';
-
-export default App;
