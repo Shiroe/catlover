f6b1f56bcd84312fd62cd3bd7342f5b05121929f
Shiroe <aggelosteck@gmail.com>
2019-10-17 03:07:16 +0300

    fixed jest conflict with sass file import

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js'
+  }
 };
